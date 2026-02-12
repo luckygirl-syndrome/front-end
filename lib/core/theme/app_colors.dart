4f0f36e10fc7cb462a3cf3ec97085b51b1046f07
PhamPham2S <kaist2015@gmail.com>
2026-02-12 19:09:16 +0900

    feat: 폰트 및 컬러 시스템 설정 완료 (#1)

--- lib/core/theme/app_colors.dart
+++ lib/core/theme/app_colors.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color primary = Color(0xFFFFC63D); 
-  static const Color secondary = Color(0xFF80B0FF);
+  // Primary
+  static const Color primaryLight = Color(0xFFFCDD7E);
+  static const Color primaryMain = Color(0xFFFFC63D);
+  static const Color primaryDark = Color(0xFFDDA600);
 
-  static const Color white0 = Color(0xFFFCFCFC);
+  // Secondary
+  static const Color secondaryLight = Color(0xFFA6D1FF);
+  static const Color secondaryMain = Color(0xFF80B0FF);
+  static const Color secondaryDark = Color(0xFF6B9AE7);
 
-  static const Color black0 = Color(0xFF1A1A1A);
-
-  static const Color grey0 = Color(0xFFCCCCCC);
+  // DarkScale
+  static const Color white = Color(0xFFFCFCFC);
+  static const Color paleGrey = Color(0xFFF2F2F2);
+  static const Color lightGrey = Color(0xFFCCCCCC);
+  static const Color grey = Color(0xFFA0A0A0);
+  static const Color darkerGrey = Color(0xFF808080);
+  static const Color black = Color(0xFF1A1A1A);
 }
--- lib/features/auth/screens/signup_screen.dart
+++ lib/features/auth/screens/signup_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
-import '../../../core/theme/app_colors.dart';
 import '../../../core/theme/app_text_styles.dart';
 
 class SignupScreen extends ConsumerWidget {
